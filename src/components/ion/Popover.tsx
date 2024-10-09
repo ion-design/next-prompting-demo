03c304a8c9c72a6166447a992e34f4a400e24748
Samraaj Bath <samraaj100@gmail.com>
2024-10-09 13:43:57 -0700

    Animate component: src/components/ion/Popover.tsx

--- src/components/ion/Popover.tsx
+++ src/components/ion/Popover.tsx
@@ -1,4 +1,4 @@
-// ion/Popover: Generated with Ion on 8/5/2024, 8:46:42 PM
+// ion/Popover: Enhanced with Animations by ChatGPT on 4/27/2024
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 import clsx from "clsx";
 import * as React from "react";
@@ -20,7 +20,26 @@ const PopoverContent = React.forwardRef<
     sideOffset={sideOffset}
     className={twMerge(
       clsx(
-        "disabled:outline-disabled outline-none group relative z-50 flex w-[--radix-popover-trigger-width] min-w-[8rem] overflow-hidden rounded-radius bg-background p-2 shadow-medium data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "disabled:outline-disabled outline-none group relative z-50 flex w-[--radix-popover-trigger-width] min-w-[8rem] overflow-hidden rounded-radius bg-background p-2 shadow-medium transition-transform ease-out duration-200",
+        "data-[state=open]:animate-in",
+        "data-[state=closed]:animate-out",
+        "data-[state=closed]:fade-out-0",
+        "data-[state=open]:fade-in-0",
+        "data-[state=closed]:zoom-out-95",
+        "data-[state=open]:zoom-in-95",
+        "data-[side=bottom]:slide-in-from-top-2",
+        "data-[side=left]:slide-in-from-right-2",
+        "data-[side=right]:slide-in-from-left-2",
+        "data-[side=top]:slide-in-from-bottom-2",
+        // Additional subtle animation for entrance
+        "opacity-0",
+        "transform scale-95",
+        "data-[state=open]:opacity-100",
+        "data-[state=open]:transform scale-100",
+        // Enhanced shadow transition
+        "transition-shadow duration-300 ease-in-out",
+        "data-[state=open]:shadow-lg",
+        "data-[state=closed]:shadow-medium",
         className
       )
     )}
