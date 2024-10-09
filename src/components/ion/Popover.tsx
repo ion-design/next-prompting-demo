6b1eeced6b9946130d9f95ac27e6a023eb9abbe2
Samraaj Bath <samraaj100@gmail.com>
2024-10-09 12:34:54 -0700

    Animate component: src/components/ion/Popover.tsx

--- src/components/ion/Popover.tsx
+++ src/components/ion/Popover.tsx
@@ -1,4 +1,5 @@
-// ion/Popover: Generated with Ion on 8/5/2024, 8:46:42 PM
+```tsx
+// ion/Popover: Enhanced with subtle animations for a luxury feel
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 import clsx from "clsx";
 import * as React from "react";
@@ -20,7 +21,7 @@ const PopoverContent = React.forwardRef<
     sideOffset={sideOffset}
     className={twMerge(
       clsx(
-        "disabled:outline-disabled outline-none group relative z-50 flex w-[--radix-popover-trigger-width] min-w-[8rem] overflow-hidden rounded-radius bg-background p-2 shadow-medium data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "disabled:outline-disabled outline-none group relative z-50 flex w-[--radix-popover-trigger-width] min-w-[8rem] overflow-hidden rounded-radius bg-background p-2 shadow-medium transition-opacity ease-in-out duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )
     )}
@@ -30,3 +31,4 @@ const PopoverContent = React.forwardRef<
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
 export { PopoverRoot as Popover, PopoverContent, PopoverTrigger };
+```
